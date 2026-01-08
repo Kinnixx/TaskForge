e394370a448564b6333148172c35d6adc7436e83
Anaïs <anais.delattre1@gmail.com>
2026-01-08 01:03:13 +0100

    chore: improve project admin display

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Project
 
-# Register your models here.
-admin.site.register(Project)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
