e14114e383eeade94358d6be79fdc1aa858996b6
Anaïs <anais.delattre1@gmail.com>
2026-01-12 14:38:21 +0100

    feature: add project list view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.all()
+    output = ", ".join(project.name for project in projects)
+    return HttpResponse(output)
