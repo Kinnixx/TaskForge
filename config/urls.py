1dcf3b0d224bdbe7e5c811544a491441e6229915
Anaïs <anais.delattre1@gmail.com>
2026-01-17 16:37:21 +0100

    add homepage route

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from projects.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home, name="home"),
     path('projects/', include('projects.urls'))
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import project_list
 
 urlpatterns = [
-    path("", project_list, name="project_list")
+    path("", project_list, name="project_list"),
 ]
